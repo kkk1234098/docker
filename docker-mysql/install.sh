474d40299e3c315d055bf4f15d590a480dcf8c0c
hc <hanchao@parkson.com.cn>
2020-09-21 16:22:40 +0800

    2020/9/21-hc

--- docker-mysql/install.sh
+++ docker-mysql/install.sh
@@ -1,15 +1,6 @@
 sh build.sh;
-
 docker-compose up -d  mysql-master;
-
 sleep 3;
-
 docker-compose up -d  mysql-slave;
-
 sleep 3;
-
-docker-compose up -d  mysql-slave2;
-
-sleep 3;
-
 docker-compose up -d atlas;
